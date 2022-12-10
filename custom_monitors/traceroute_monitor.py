a6cc32be8d661b4ee1a32a66515870909a761cf6
Tomaz Muraus <tomaz@tomaz.me>
2022-12-10 20:46:13 +0100

    Don't log anything on non-fatal error.

--- custom_monitors/traceroute_monitor.py
+++ custom_monitors/traceroute_monitor.py
@@ -153,8 +153,9 @@ class TracerouteMonitor(ScalyrMonitor):
             try:
                 node_rtt = node["links"][-1][-1]["probes"][-1]["replies"][-1]["rtt"]
             except KeyError:
-                self._logger.warn(f"Failed to parse node links", exc_info=True)
-                self._logger.warn(f"Output: {node['links'][-1][-1]}")
+                continue
+                #self._logger.warn(f"Failed to parse node links", exc_info=True)
+                #self._logger.warn(f"Output: {node['links'][-1][-1]}")
                 continue
 
             hop_rtts.append(node_rtt)
